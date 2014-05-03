dc70880e736a7eac4579e841c5f5f76d026ed6d3
Emerson Dário <emersonrodrigod@gmail.com>
2014-05-03 11:34:05 -0300

    Exercício 1 de javascript

---
+++ javascript/exercicio1/exercicio.js
@@ -0,0 +1,17 @@
+/*
+ * Faça um programa que receba em uma varialvel o ano 
+ * do seu nascimento e calcule a sua idade;
+ * depois verifique se a idade for maior do que 18 anos 
+ * mostre você é maior de idade se for menor de 18 anos escreva 
+ * você é menor de idade
+ */
+
+var anoNascimento = 2000;
+var idade = 2014 - anoNascimento;
+
+if(idade < 18){
+    alert("Você é menor de idade");
+}else {
+    alert("Você é maior de idade");
+}
+
