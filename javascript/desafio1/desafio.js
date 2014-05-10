526821cf15d59ce7e42fd7fb4f22f4cf7904a77c
Emerson Dário <emersonrodrigod@gmail.com>
2014-05-10 09:31:52 -0300

    Repetições javascript

---
+++ javascript/desafio1/desafio.js
@@ -0,0 +1,19 @@
+var nome = "batatinha";
+var idade = 42;
+var sexo = "m";
+
+if (sexo == "m") {
+    if (idade >= 18) {
+        console.log("Senhor " + nome + ", seja bem vindo! Você pode entrar na festa!");
+    } else {
+        console.log("Senhor " + nome + " , você não pode entrar na festa!");
+    }
+} else {
+    if (idade >= 21) {
+        console.log("Senhorita " + nome + ", seja bem vindo! Você pode entrar na festa!");
+    } else {
+        console.log("Senhorita " + nome + " , você não pode entrar na festa!");
+    }
+}
+
+
---
+++ javascript/repeticao/repeticao.js
@@ -0,0 +1,17 @@
+/*
+ * Laço de repetição While
+ */
+
+var contador = 0;
+
+while (contador < 6){
+    console.log("Bom dia!");
+    contador ++;
+}
+
+/*
+ * Laço de repetição FOR
+ */
+for(var conta = 0; conta < 10; conta ++){
+    console.log("Boa tarde!");
+}
