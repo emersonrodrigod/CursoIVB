3e22f366ac51fc98ceab9c0b4dc19b4b50b00df4
Emerson Dário <emersonrodrigod@gmail.com>
2014-05-17 09:18:03 -0300

    Começando com DOM

--- javascript/DOM/comecando.js
+++ javascript/DOM/comecando.js
@@ -1,2 +1,36 @@
-console.log(document.getElementById("titulo"));
+
+/*
+ * Método getElementById 
+ * Retorna um único elemento
+ * */
+var elemento = document.getElementById('titulo');
+console.log(elemento);
+
+/*
+ * getElementsByClassName
+ * retorna um array de objetos que satisfaçam a condição
+ */
+var arrayClasses = document.getElementsByClassName('texto');
+console.log(arrayClasses);
+
+/*
+ * Selecionar pelo elemento html
+ * getElementByTagName
+ * Retorna um array de objetos que satisfaçam a condição
+ */
+var h1 = document.getElementsByTagName('h1');
+console.log(h1);
+
+/**
+ * selecionar por critério
+ * querySelector
+ * Retorna um elemento que satisfaça a condição
+ */
+var textoSelecionado = document.querySelector('#conteudo .texto');
+console.log(textoSelecionado);
+
+
+
+
+
 
